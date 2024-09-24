a73eb93569ede3c1d1fd777b48c2cc3f5797ca03
ntl-y <n-danilkina@list.ru>
2024-09-24 21:53:01 +0000

    simple requests

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,85 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	server     = "api.vk.ru"
+	methodName = "video.get"
+	apiVersion = "5.199"
+)
+
+func gatherParams() url.Values {
+	params := url.Values{}
+
+	params.Add("q", "майнкрафт")
+	// params.Add("sort", "0")
+	// params.Add("adult", "1")
+	params.Add("filters", "vk")
+	// params.Add("search_own", "0")
+	// params.Add("offset", "0") // смещение
+	params.Add("count", "200")
+	params.Add("longer", "5")
+	// params.Add("shorter", "6000")
+	params.Add("extended", "0")
+	params.Add("v", apiVersion)
+
+	return params
+
+}
+
+func createRequest() *http.Request {
+	bearer := "Bearer " + os.Getenv("VK_TOKEN")
+	params := gatherParams().Encode()
+
+	url := fmt.Sprintf("https://%s/method/%s?%s", server, methodName, params)
+
+	req, _ := http.NewRequest("POST", url, nil)
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/x-www-form-encoded")
+
+	return req
+}
+
+func sendRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func main() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalln(err)
+	}
+
+	client := &http.Client{}
+	req := createRequest()
+	resp, err := sendRequest(client, req)
+	if err != nil {
+		logrus.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Println(err)
+	}
+
+	err = os.WriteFile("response.json", body, 0644)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+
+	fmt.Println("Response saved to response.json")
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module hidden_gems
+
+go 1.23.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
